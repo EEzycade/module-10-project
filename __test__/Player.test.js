9070b7ad616104bf2b65c59736daa64214f81590
Cade Ellsworth <caedmd2100@gmail.com>
2021-05-11 16:35:38 -0700

    added prototypes and passed more tests

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -9,6 +9,11 @@ const Player = require('../lib/Player');
 test('creates a new player object', () => {
     const player = new Player('Dave');
 
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
@@ -17,4 +22,14 @@ test('creates a new player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
-})
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player("Dave");
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
